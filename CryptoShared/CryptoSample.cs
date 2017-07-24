d7701e094d916c3973e8aa271bb8dec8dfe5393b
Andrew Arnott <andarno@microsoft.com>
2017-07-24 10:49:10 -0700

    Add Win8 store and phone app projects

--- CryptoShared/CryptoSample.cs
+++ CryptoShared/CryptoSample.cs
@@ -13,7 +13,7 @@ namespace CryptShared
             var keyMaterial = WinRTCrypto.CryptographicBuffer.GenerateRandom(aes.LegalKeySizes.First().First());
             var key = aes.CreateSymmetricKey(keyMaterial);
             var iv = WinRTCrypto.CryptographicBuffer.GenerateRandom(aes.BlockLength);
-            byte[] plainText = Encoding.ASCII.GetBytes("Hello world");
+            byte[] plainText = Encoding.UTF8.GetBytes("Hello world");
             byte[] cipherBytes = WinRTCrypto.CryptographicEngine.Encrypt(key, plainText, iv);
             string cipherText = Convert.ToBase64String(cipherBytes);
             return cipherText;
